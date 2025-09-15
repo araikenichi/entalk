4c124ea90f8d8627a625c7b5b2a4278c5f341910
AK <i.zhenlong@icloud.com>
2025-09-15 15:22:41 +0900

    fix: set base to /nchat

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,17 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
 
   return {
-    base: '/entalk/', // GitHub Pages のサブパス
+    // GitHub Pages のサブパス（リポジトリ名）
+    base: '/nchat/',
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
-     alias: {
-  '@': path.resolve(__dirname, 'src'),
-},
-
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
     build: {
       outDir: 'dist',
